a668d79989415cee0f6c89e49ba597d933d08177
krisswiss <chrisswiatek@hotmail.com>
2020-12-21 14:05:36 +0000

    FAQ - registered faq model in admin.py

--- faq/admin.py
+++ faq/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import faq
+
 
 # Register your models here.
+
+
+admin.site.register(faq)
--- faq/models.py
+++ faq/models.py
@@ -7,4 +7,4 @@ class faq(models.Model):
     question = models.CharField(max_length=254)
     answer = models.TextField()
     def __str__(self):
-        return self.name
+        return self.question
