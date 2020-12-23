0255da608e652d3054f82d5b4aaea4b1193d0eb4
krisswiss <chrisswiatek@hotmail.com>
2020-12-23 16:21:45 +0000

    FAQ - added form

--- faq/views.py
+++ faq/views.py
@@ -11,3 +11,10 @@ def all_faq(request):
     }
     return render(request, 'faq/faq.html', context)
 
+
+def edit_faq(request, pk):
+    question = Faq.objects.get(pk=pk)
+    context = {
+        'question': question,
+    }
+    return render(request, 'faq/edit_faq.html', context)
