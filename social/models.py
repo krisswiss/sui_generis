72a07018ff66f7b91d900f7318213352ca25086d
krisswiss <chrisswiatek@hotmail.com>
2020-12-28 15:14:45 +0000

    SOCIAL - added model

--- social/models.py
+++ social/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Social(models.Model):
+    name = models.CharField(max_length=50)
+    weblink = models.URLField()
+    icon = models.CharField(max_length=20)
+    def __str__(self):
+        return self.question
