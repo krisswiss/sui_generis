6750f1ef70ff08951729bfc883246c57bcd9dfd9
krisswiss <chrisswiatek@hotmail.com>
2020-12-28 18:52:11 +0000

    Removed secret key and changed debug

--- sui_generis/settings.py
+++ sui_generis/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1o7aomdp$-ji_^ddqojf_k#coia26-hw+(5)0fj5$=ou85%4qu'
+SECRET_KEY = os.environ.get('SECRET_KEY_2', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "DEVELOPMENT" in os.environ
 
 ALLOWED_HOSTS = ['sui-generis.herokuapp.com', 'localhost']
 
