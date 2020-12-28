3e6f29042a70a8d2f1ee50244ecede1efe124fa8
krisswiss <chrisswiatek@hotmail.com>
2020-12-28 00:08:31 +0000

    EDIT_QUESTION - added post handler to the view and link into template to update and delete questions

--- faq/views.py
+++ faq/views.py
@@ -22,7 +22,7 @@ def add_question(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added question!')
-            return redirect(reverse('all_faq'))
+            return redirect('all_faq')
         else:
             messages.error(request, 'Failed to add question. Please ensure the form is valid.')
     else:
@@ -39,10 +39,21 @@ def add_question(request):
 def edit_question(request, faq_id):
     """ Edit question in FAQ """
     question = get_object_or_404(Faq, pk=faq_id)
-    form = FaqForm(instance=question)
+    if request.method == 'POST':
+        form = FaqForm(request.POST, request.FILES, instance=question)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Successfully updated question!')
+            return redirect('all_faq')
+        else:
+            messages.error(request, 'Failed to update question. Please ensure the form is valid.')
+    else:
+        form = FaqForm(instance=question)
+        messages.info(request, f'You are editing question: "{question.question}"')
 
+    template = 'faq/edit_question.html'
     context = {
         'form': form,
         'question': question,
     }
-    return render(request, 'faq/edit_question.html', context)
+    return render(request, template, context)
