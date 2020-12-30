a66bbe5541e3b346c8dad066161034c686b49729
krisswiss <chrisswiatek@hotmail.com>
2020-12-30 17:12:27 +0000

    CONTACT - added form

---
+++ contact/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+
+class ContactForm(forms.Form):
+    """
+    A form for contact
+    """
+
+    full_name = forms.CharField(
+         label="Full Name"
+    )
+    email = forms.EmailField(
+        label="Email"
+    )
+    message = forms.CharField(
+        label="Message",
+        widget=forms.Textarea(attrs={
+            "rows": 8,
+        })
+    )
+
+    class Meta:
+        fields = ['full_name', 'email', 'message']
+    
