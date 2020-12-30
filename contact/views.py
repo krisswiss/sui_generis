dae18ceba62849de43fea0cc89587c10db9c650a
krisswiss <chrisswiatek@hotmail.com>
2020-12-30 17:42:17 +0000

    CONTACT - views imports corrected

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,9 @@
-import os
-
 from django.shortcuts import render, redirect
 from django.core.mail import send_mail, BadHeaderError
 from django.conf import settings
 from django.http import HttpResponse
 from .forms import ContactForm
-from profiles.models import Profile
+from profiles.models import UserProfile
 
 
 # Create your views here.
@@ -25,7 +23,7 @@ def contact(request):
             try:
                 send_mail(
                     # to capture the users name and email it's displayd in subject field and can be responded to
-                    f"Message from {full_name}, <{user_email}>", 
+                    f"Message from {full_name}, <{user_email}>",
                     message,
                     user_email,
                     [settings.DEFAULT_FROM_EMAIL],
@@ -38,7 +36,7 @@ def contact(request):
         # Attempt to prefill email field for logged in user, if they have
         # this information saved in the profile
         if request.user.is_authenticated:
-            profile = Profile.objects.get(user=request.user)
+            profile = UserProfile.objects.get(user=request.user)
             user_email = profile.user.email
             contact_form = ContactForm(initial={
                 'email': user_email,
