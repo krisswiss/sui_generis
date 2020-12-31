3024db8eb130a54e3a78f49d56c79184a93a2486
krisswiss <chrisswiatek@hotmail.com>
2020-12-31 01:50:35 +0000

    BLOG - added urls

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.all_posts, name='all_posts'),
+    path('<int:post_id>/', views.each_post, name='each_post'),
+    path('add_pos/', views.add_post, name='add_post'),
+    path('edit_post/<int:post_id>/', views.edit_post, name='edit_post'),
+    path('delete_post/<int:faq_id>/', views.delete_post, name='delete_post'),
+]
--- sui_generis/urls.py
+++ sui_generis/urls.py
@@ -28,4 +28,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('faq/', include('faq.urls')),
     path('contact/', include('contact.urls')),
+    path('blog/', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
