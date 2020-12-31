181d694823792bbd9196243f6cddf5a03245ce45
krisswiss <chrisswiatek@hotmail.com>
2020-12-31 00:49:59 +0000

    BLOG - created model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=254)
+    introduction = models.CharField(max_length=254)
+    post = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+    date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
