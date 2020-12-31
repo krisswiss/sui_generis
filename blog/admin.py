d58963d30ffeab5559206198fdbbc082dc24f80f
krisswiss <chrisswiatek@hotmail.com>
2020-12-31 00:58:19 +0000

    BLOG - admin done

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+        'date',
+    )
+
+
+admin.site.register(Post, PostAdmin)
